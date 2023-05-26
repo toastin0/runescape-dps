8322e2c2808e9b01ba3696e55119f52b77158f69
toast <64383025+toastin0@users.noreply.github.com>
2023-05-25 23:41:36 -0400

    this doesn't work but i'm worried about changes
    no

--- src/app/Prayergrid.tsx
+++ src/app/Prayergrid.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Prayer from "./Prayer"
+import style from './Prayer.module.css'
 export default function Prayergrid({prayerBook}: {prayerBook: "normal" | "ruinous"}) {
   // i don't think there is a way to programmatically import
   // many images at once, so i am pretty sure that i just have to
@@ -7,51 +8,52 @@ export default function Prayergrid({prayerBook}: {prayerBook: "normal" | "ruinou
   // and choose not to blame myself
   const normalBook = 
     (
-    <div className="prayer-list">
-      <div className="prayer-column">
-        <Prayer src="/prayers/Thick_Skin.png" alt="Thick Skin"></Prayer>
-        <Prayer src="/prayers/Burst_of_Strength.png" alt="Burst of Strength"></Prayer>
-        <Prayer src="/prayers/Clarity_of_Thought.png" alt="Clarity of Thought"></Prayer>
-        <Prayer src="/prayers/Sharp_Eye.png" alt="Sharp Eye"></Prayer>
-        <Prayer src="/prayers/Mystic_Will.png" alt="Mystic Will"></Prayer>
-      </div>
-      <div className="prayer-column">
-        <Prayer src="/prayers/Rock_Skin.png" alt="Rock Skin"></Prayer>
-        <Prayer src="/prayers/Superhuman_Strength.png" alt="Superhuman Strength"></Prayer>
-        <Prayer src="/prayers/Improved_Reflexes.png" alt="Improved Reflexes"></Prayer>
-        <Prayer src="/prayers/Rapid_Restore.png" alt="Rapid Restore"></Prayer>
-        <Prayer src="/prayers/Rapid_Heal.png" alt="Rapid Heal"></Prayer>
-      </div>
-      <div className="prayer-column">
-        <Prayer src="/prayers/Protect_Item.png" alt="Protect Item"></Prayer>
-        <Prayer src="/prayers/Hawk_Eye.png" alt="Hawk Eye"></Prayer>
-        <Prayer src="/prayers/Mystic_Lore.png" alt="Mystic Lore"></Prayer>
-        <Prayer src="/prayers/Steel_Skin.png" alt="Steel Skin"></Prayer>
-        <Prayer src="/prayers/Ultimate_Strength.png" alt="Ultimate Strength"></Prayer>
-      </div>
-      <div className="prayer-column">
-        <Prayer src="/prayers/Incredible_Reflexes.png" alt="Incredible Reflexes"></Prayer>
-        <Prayer src="/prayers/Protect_from_Magic.png" alt="Protect from Magic"></Prayer>
-        <Prayer src="/prayers/Protect_from_Missiles.png" alt="Protect from Missiles"></Prayer>
-        <Prayer src="/prayers/Protect_from_Melee.png" alt="Protect from Melee"></Prayer>
-        <Prayer src="/prayers/Eagle_Eye.png" alt="Eagle Eye"></Prayer>
-      </div>
-      <div className="prayer-column">
-        <Prayer src="/prayers/Mystic_Might.png" alt="Mystic Might"></Prayer>
-        <Prayer src="/prayers/Retribution.png" alt="Retribution"></Prayer>
-        <Prayer src="/prayers/Redemption.png" alt="Redemption"></Prayer>
-        <Prayer src="/prayers/Smite.png" alt="Smite"></Prayer>
-        <Prayer src="/prayers/Preserve.png" alt="Preserve"></Prayer>
-      </div>
-      <div className="prayer-column">
-        <Prayer src="/prayers/Chivalry.png" alt="Chivalry"></Prayer>
-        <Prayer src="/prayers/Piety.png" alt="Piety"></Prayer>
-        <Prayer src="/prayers/Rigour.png" alt="Rigour"></Prayer>
-        <Prayer src="/prayers/Augury.png" alt="Augury"></Prayer>
-      </div>
+    <div id="prayer-controls" className={style.prayerBackground}>
+      <div className={style.prayerList}>
+        <div className="prayer-column">
+          <Prayer src="/prayers/Thick_Skin.png" alt="Thick Skin"></Prayer>
+          <Prayer src="/prayers/Burst_of_Strength.png" alt="Burst of Strength"></Prayer>
+          <Prayer src="/prayers/Clarity_of_Thought.png" alt="Clarity of Thought"></Prayer>
+          <Prayer src="/prayers/Sharp_Eye.png" alt="Sharp Eye"></Prayer>
+          <Prayer src="/prayers/Mystic_Will.png" alt="Mystic Will"></Prayer>
+        </div>
+        <div className="prayer-column">
+          <Prayer src="/prayers/Rock_Skin.png" alt="Rock Skin"></Prayer>
+          <Prayer src="/prayers/Superhuman_Strength.png" alt="Superhuman Strength"></Prayer>
+          <Prayer src="/prayers/Improved_Reflexes.png" alt="Improved Reflexes"></Prayer>
+          <Prayer src="/prayers/Rapid_Restore.png" alt="Rapid Restore"></Prayer>
+          <Prayer src="/prayers/Rapid_Heal.png" alt="Rapid Heal"></Prayer>
+        </div>
+        <div className="prayer-column">
+          <Prayer src="/prayers/Protect_Item.png" alt="Protect Item"></Prayer>
+          <Prayer src="/prayers/Hawk_Eye.png" alt="Hawk Eye"></Prayer>
+          <Prayer src="/prayers/Mystic_Lore.png" alt="Mystic Lore"></Prayer>
+          <Prayer src="/prayers/Steel_Skin.png" alt="Steel Skin"></Prayer>
+          <Prayer src="/prayers/Ultimate_Strength.png" alt="Ultimate Strength"></Prayer>
+        </div>
+        <div className="prayer-column">
+          <Prayer src="/prayers/Incredible_Reflexes.png" alt="Incredible Reflexes"></Prayer>
+          <Prayer src="/prayers/Protect_from_Magic.png" alt="Protect from Magic"></Prayer>
+          <Prayer src="/prayers/Protect_from_Missiles.png" alt="Protect from Missiles"></Prayer>
+          <Prayer src="/prayers/Protect_from_Melee.png" alt="Protect from Melee"></Prayer>
+          <Prayer src="/prayers/Eagle_Eye.png" alt="Eagle Eye"></Prayer>
+        </div>
+        <div className="prayer-column">
+          <Prayer src="/prayers/Mystic_Might.png" alt="Mystic Might"></Prayer>
+          <Prayer src="/prayers/Retribution.png" alt="Retribution"></Prayer>
+          <Prayer src="/prayers/Redemption.png" alt="Redemption"></Prayer>
+          <Prayer src="/prayers/Smite.png" alt="Smite"></Prayer>
+          <Prayer src="/prayers/Preserve.png" alt="Preserve"></Prayer>
+        </div>
+        <div className="prayer-column">
+          <Prayer src="/prayers/Chivalry.png" alt="Chivalry"></Prayer>
+          <Prayer src="/prayers/Piety.png" alt="Piety"></Prayer>
+          <Prayer src="/prayers/Rigour.png" alt="Rigour"></Prayer>
+          <Prayer src="/prayers/Augury.png" alt="Augury"></Prayer>
+        </div>
 
       </div>
-  
+    </div>
   )
   
   const ruinousBook = <></> 
