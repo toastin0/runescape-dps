38465a931a0f9ea00910e58ed7a2566ab376d2e5
toast <64383025+toastin0@users.noreply.github.com>
2023-05-25 22:21:20 -0400

    i got the grid right, so take note of this css

---
+++ src/app/Prayer.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import Image from 'next/image';
+import styles from './Prayer.module.css'
+
+export default function Prayer({ src, isActive = false, alt }: {src: string, isActive?: boolean, alt: string}){
+   return (
+   <Image src={src} alt={alt} width={30} height={30} 
+   className={isActive ? styles.prayerimage : `${styles.prayerimage} ${styles.clicked}`} 
+   />
+   )
+
+}
