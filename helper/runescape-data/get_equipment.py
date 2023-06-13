a73048ff66d8b46c560d95e54b480a30769da6ee
toast <64383025+toastin0@users.noreply.github.com>
2023-06-12 22:27:31 -0400

    python kind of working, but would like to improve it

---
+++ helper/runescape-data/get_equipment.py
@@ -0,0 +1,44 @@
+import requests
+import time
+import json
+import collections
+
+# MAX_OFFSET = 3520
+MAX_OFFSET = 100
+API_URL = "https://oldschool.runescape.wiki/api.php"
+header = {"User-Agent": "runescape-equipment-data-puller/0.0.1"}
+offset = 0
+main_dict = dict()
+with open("./helper/runescape-data/item_list.json", "w") as file:
+    while offset <= MAX_OFFSET:
+        time.sleep(1)
+        print(f"current offset: {offset}")
+        parameters = {
+            "action": "askargs",
+            "conditions": "Category:Equipable items",
+            "printouts": "All Combat style |All Crush attack bonus |All Crush defence bonus |All Equipment slot |All Magic Damage bonus |All Magic attack bonus |All Magic defence bonus |All Prayer bonus |All Range attack bonus |All Range defence bonus |All Slash attack bonus |All Slash defence bonus |All Stab attack bonus |All Stab defence bonus |All Strength bonus |All Weapon attack range |All Weapon attack speed |All Item ID |Image",
+            "parameters": f"offset={offset}",
+            "format": "json",
+            "api_version": 3
+        }
+        response = requests.get(API_URL, params=parameters, headers=header)
+        print(response.url)
+        if response.ok:
+            json_response: dict = response.json()
+            for entry in json_response["query"]["results"]:
+                main_dict.update(entry)
+            if "query-continue-offset" in json_response:
+                offset = json_response["query-continue-offset"]
+            else: 
+                break
+    file.write(json.dumps(main_dict))
+
+# TODO: remove the single entry lists found in helper/runescape-data/item_list.json
+# these would be really annoying to have!
+def remove_single_entry_lists(input: dict):
+    for (key, value) in dict.items():
+        if type(value) is list:
+            if len(value) == 1:
+                input.update({key: value[0]})
+         
+
